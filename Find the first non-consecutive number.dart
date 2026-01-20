41730d1590f62294722574a481126f2411c3cba4
Ahmed Omar <omar155432@gmai.com>
2026-01-20 18:28:40 +0200

    Find the first non-consecutive number

---
+++ Find the first non-consecutive number.dart
@@ -0,0 +1,13 @@
+
+int? firstNonConsecutive(List<int> arr) {
+  if (arr.length <= 1) return null;
+
+  for (var i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] != arr[i] + 1) {
+      return arr[i + 1];
+    }
+  }
+  return null;
+}
+
+
